f4ad74c3ec976f02e2f67bf82bcf4657d93b8fb9
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2025-12-12 23:17:40 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ KanhaMusic/utils/exceptions.py
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2025-2026 by OyeKanhaa@Github, < https://github.com/OyeKanhaa >.
+#
+# This file is part of < https://github.com/OyeKanhaa/KanhaMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/OyeKanhaa/KanhaMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
+
+class AssistantErr(Exception):
+    def __init__(self, errr: str):
+        super().__init__(errr)
