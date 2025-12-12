ad9efdf9116d26614983f5ddd1a281445bd49138
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2025-12-12 23:17:15 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ KanhaMusic/__init__.py
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2025-2026 by OyeKanhaa@Github, < https://github.com/OyeKanhaa >.
+#
+# This file is part of < https://github.com/OyeKanhaa/KanhaMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/OyeKanhaa/KanhaMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from KanhaMusic.core.bot import Kanha
+from KanhaMusic.core.dir import dirr
+from KanhaMusic.core.userbot import Userbot
+from KanhaMusic.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+dbb()
+heroku()
+
+app = Kanha()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
