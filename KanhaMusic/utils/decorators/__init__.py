56cda2ab2af3aa1149234aa1ca88781c3d6dd9c8
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2025-12-12 23:18:00 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ KanhaMusic/utils/decorators/__init__.py
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2025-2026 by OyeKanhaa@Github, < https://github.com/OyeKanhaa >.
+#
+# This file is part of < https://github.com/OyeKanhaa/KanhaMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/OyeKanhaa/KanhaMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from .admins import *
+from .language import *
