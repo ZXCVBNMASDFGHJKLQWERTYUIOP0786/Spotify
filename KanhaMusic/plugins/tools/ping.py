676cb2d225ddab680445b455179f5ff63ee6a045
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2025-12-12 23:18:54 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ KanhaMusic/plugins/tools/ping.py
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2025-2026 by OyeKanhaa@Github, < https://github.com/OyeKanhaa >.
+#
+# This file is part of < https://github.com/OyeKanhaa/KanhaMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/OyeKanhaa/KanhaMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from KanhaMusic import app
+from KanhaMusic.core.call import Kanha
+from KanhaMusic.utils import bot_sys_stats
+from KanhaMusic.utils.decorators.language import language
+from KanhaMusic.utils.inline import supp_markup
+from config import BANNED_USERS, PING_IMG_URL
+
+
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"].format(app.mention),
+    )
+    pytgping = await Kanha.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
+    )
