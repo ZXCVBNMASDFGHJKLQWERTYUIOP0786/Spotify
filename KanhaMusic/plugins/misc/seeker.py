a09f9d50e839dd1f863f6fce73337e79363aa1bc
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2025-12-12 23:19:17 +0530

    ˹ sʜʀᴇᴇ ꭙ ϻᴜsɪᴄ ˼

---
+++ KanhaMusic/plugins/misc/seeker.py
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2025-2026 by OyeKanhaa@Github, < https://github.com/OyeKanhaa >.
+#
+# This file is part of < https://github.com/OyeKanhaa/KanhaMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/OyeKanhaa/KanhaMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
+import asyncio
+
+from KanhaMusic.misc import db
+from KanhaMusic.utils.database import get_active_chats, is_music_playing
+
+
+async def timer():
+    while not await asyncio.sleep(1):
+        active_chats = await get_active_chats()
+        for chat_id in active_chats:
+            if not await is_music_playing(chat_id):
+                continue
+            playing = db.get(chat_id)
+            if not playing:
+                continue
+            duration = int(playing[0]["seconds"])
+            if duration == 0:
+                continue
+            if db[chat_id][0]["played"] >= duration:
+                continue
+            db[chat_id][0]["played"] += 1
+
+
+asyncio.create_task(timer())
