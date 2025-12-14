c54d1f260e8bc90378287b1fb18463cce1623267
Rishu <vishwakarmarishabh366@gmail.com>
2025-12-14 19:11:26 +0530

    Update help.py

--- KanhaMusic/utils/inline/help.py
+++ KanhaMusic/utils/inline/help.py
@@ -97,3 +97,20 @@ def private_help_panel(_):
         ],
     ]
     return buttons
+
+
+
+def Support_panel(_):
+    buttons = [
+             [
+InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+             ],
+             [
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+             ],
+             [
+InlineKeyboardButton(text=_["BACK_BUTTON"],
+callback_data="settings_back_helper")]
+    ]
+    return buttons
