b6123f9bdd3b1450bc24564d9815fb0ab41bd5e5
Rishu <vishwakarmarishabh366@gmail.com>
2025-12-14 19:12:16 +0530

    Update help.py

--- KanhaMusic/utils/inline/help.py
+++ KanhaMusic/utils/inline/help.py
@@ -1,18 +1,10 @@
-#
-# Copyright (C) 2025-2026 by OyeKanhaa@Github, < https://github.com/OyeKanhaa >.
-#
-# This file is part of < https://github.com/OyeKanhaa/KanhaMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/OyeKanhaa/KanhaMusic/blob/master/LICENSE >
-#
-# All rights reserved.
 
 from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+import config
 from KanhaMusic import app
-
 
 def help_pannel(_, START: Union[bool, int] = None):
     first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")]
