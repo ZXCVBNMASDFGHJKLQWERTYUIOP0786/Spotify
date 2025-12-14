14f949839607680cfb34ae5ea315dfa5a58fda60
Rishu <vishwakarmarishabh366@gmail.com>
2025-12-14 20:30:39 +0530

    Update start.py

--- KanhaMusic/utils/inline/start.py
+++ KanhaMusic/utils/inline/start.py
@@ -36,7 +36,7 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="about_read"),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_SUPPORT),
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
