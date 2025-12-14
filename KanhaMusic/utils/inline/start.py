6db59473cd9b66fa846fbe3f8c1c859ad7d7e1c2
Rishu <vishwakarmarishabh366@gmail.com>
2025-12-14 18:42:09 +0530

    Update start.py

--- KanhaMusic/utils/inline/start.py
+++ KanhaMusic/utils/inline/start.py
@@ -1,11 +1,4 @@
-#
-# Copyright (C) 2025-2026 by OyeKanhaa@Github, < https://github.com/OyeKanhaa >.
-#
-# This file is part of < https://github.com/OyeKanhaa/KanhaMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/OyeKanhaa/KanhaMusic/blob/master/LICENSE >
-#
-# All rights reserved.
+
 
 from pyrogram.types import InlineKeyboardButton
 
@@ -18,8 +11,10 @@ def start_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
+            )],
+             [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
     ]
     return buttons
