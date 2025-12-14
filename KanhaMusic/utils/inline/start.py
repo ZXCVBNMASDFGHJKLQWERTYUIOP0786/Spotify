8e10eb9cfe8e9f0b19730537b6bcfada35e29585
Rishu <vishwakarmarishabh366@gmail.com>
2025-12-14 19:31:16 +0530

    Update start.py

--- KanhaMusic/utils/inline/start.py
+++ KanhaMusic/utils/inline/start.py
@@ -1,43 +1,62 @@
-
-
-from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 import config
 from KanhaMusic import app
 
 
 def start_panel(_):
-    buttons = [
+    return InlineKeyboardMarkup(
         [
-            InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            )],
-             [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-        ],
-    ]
-    return buttons
+            [
+                InlineKeyboardButton(
+                    text=_["S_B_1"],
+                    url=f"https://t.me/{app.username}?startgroup=true",
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["S_B_2"],
+                    url=config.SUPPORT_CHAT,
+                ),
+                InlineKeyboardButton(
+                    text=_["S_B_6"],
+                    url=config.SUPPORT_CHANNEL,
+                ),
+            ],
+        ]
+    )
 
 
 def private_panel(_):
-    buttons = [
+    return InlineKeyboardMarkup(
         [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"],                     callback_data="help_callback llpport"),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-        ],
-    ]
-    return buttons
-
-
-
+            [
+                InlineKeyboardButton(
+                    text=_["S_B_3"],
+                    url=f"https://t.me/{app.username}?startgroup=true",
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["S_B_4"],
+                    callback_data="settings_back_helper",
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["S_B_5"],
+                    user_id=config.OWNER_ID,
+                ),
+                InlineKeyboardButton(
+                    text=_["S_B_6"],
+                    callback_data="help_callback llpport",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["S_B_6"],
+                    url=config.SUPPORT_CHANNEL,
+                ),
+            ],
+        ]
+    )
