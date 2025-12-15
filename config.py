2f817cb2a7b4110d7ad9bd459f4e0b2f80d80de5
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2025-12-15 13:19:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,9 +43,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/OyeKanhaa/KanhaMusic",
+    "https://github.com/ZXCVBNMASDFGHJKLQWERTYUIOP0786/Spotify",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
