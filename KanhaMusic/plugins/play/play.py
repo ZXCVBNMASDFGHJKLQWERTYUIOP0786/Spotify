8fb9db227e2813d5be04d15c860108dec34ed185
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2025-12-15 19:55:39 +0530

    Update play.py

--- KanhaMusic/plugins/play/play.py
+++ KanhaMusic/plugins/play/play.py
@@ -68,6 +68,13 @@ async def play_commnd(
     url,
     fplay,
 ):
+
+sticker_msg = await message.reply_sticker(
+        "CAACAgUAAyEFAASOlzVAAAEBbMVoZBvc22oR8X-QlMPpERj8bdrDtgAChAsAAjXBOFddqD7hjDYLoh4E"
+    )
+
+    asyncio.create_task(delete_after_delay(sticker_msg))
+
     mystic = await message.reply_text(
         _["play_2"].format(channel) if channel else _["play_1"]
     )
